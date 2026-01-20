4a4c6b6366d47dc2e6b450ff7bc8461481c3abea
logeshv517 <vlogesh517@gmail.com>
2026-01-21 02:02:30 +0530

    added related products suggestions and cart logic implementation in-progress

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,12 @@ import Navbar  from './components/Navbar'
 import Landing from './pages/Landing'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+
 const App = () => {
   return (
     <div  className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <ToastContainer/>
         <Navbar />
         <SearchBar/>
         <Routes>
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { products } from '../assets/assets';
+import { toast } from 'react-toastify';
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
@@ -9,10 +10,50 @@ const ShopContextProvider = (props) => {
 
     const[search,setSearch] = useState('');
     const[showSearch, setShowSearch] = useState(false);
+    const[cartItems,setCartItems] = useState({});
+
+    const addToCart = async(itemId,size) =>{
+
+        if(!size){
+            toast.error('Select Product Size');
+            return;
+        }
+
+        let cardData = structuredClone(cartItems);
+        if(cardData[itemId]){
+            if(cardData[itemId][size]){
+                cardData[itemId][size] +=1;
+            }else{
+                cardData[itemId][size] = 1;
+            }
+        }else{
+            cardData[itemId] = {};
+            cardData[itemId][size] = 1;
+        }
+
+        setCartItems(cardData);
+    }
+
+    const getCartCount = () =>{
+        let count = 0;
+        for(const items in  cartItems){
+            for(const item in cartItems[items]){
+                try{
+                    if(cartItems[items][item]>0){
+                        count+=cartItems[items][item];
+                    }
+                }catch(error){
+
+                }
+            }
+        }
+        return count;
+    }
 
     const value = {
         products, currency, delivery_fee,
-        search,setSearch,showSearch,setShowSearch
+        search,setSearch,showSearch,setShowSearch,
+        cartItems,addToCart,getCartCount
     }
     return (
         <ShopContext.Provider value={value}>
